a216bd20b79a7516daefc3a48fc026a00eff48e9
Maria <cottiermaria@myocu.oak.edu>
2021-05-16 23:26:19 -0500

    Icons, Favorites, and Comments

---
+++ components/ContactComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+
+class Contact extends Component {
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+          <Text>
+            1 Nucamp Way{"\n"}
+            Seattle, WA 98001 {"\n"}
+            U.S.A. {"\n"}
+          </Text>
+          <Text style={{ marginBottom: 10 }}>
+            Phone: 1-206-555-1234 {"\n"}
+            Email: campsites@nucamp.co
+          </Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;
